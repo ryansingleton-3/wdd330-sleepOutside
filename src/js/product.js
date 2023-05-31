aec5be233ba4f1816b519ac04d0c91064d771eea
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-05-31 16:05:40 -0400

    finished team activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,7 @@
-import { setLocalStorage, getParam} from "./utils.mjs";
+import { setLocalStorage, getParam } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
-import { productDetails } from "./productDetails.mjs"
+import { productDetails } from "./productDetails.mjs";
 
 const productId = getParam("product");
 console.log(findProductById(productId));
-productDetails(productId)
-
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-
+productDetails(productId);
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -6,31 +6,31 @@ import { setLocalStorage } from "./utils.mjs";
 let product = {};
 
 export default async function productDetails(productId) {
-    // get the details for the current product. findProductById will return a promise! use await or .then() to process it
-    product = await findProductById(productId);
-    console.log(product)
-    // once we have the product details we can render out the HTML
-    renderProductDetails(product);
-    // once the HTML is rendered we can add a listener to Add to Cart button
-    document.getElementById("addToCart").addEventListener("click", addToCart);
-  }
-  
-  
+  // get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  product = await findProductById(productId);
+  console.log(product);
+  // once we have the product details we can render out the HTML
+  renderProductDetails(product);
+  // once the HTML is rendered we can add a listener to Add to Cart button
+  document.getElementById("addToCart").addEventListener("click", addToCart);
+}
+
+function addToCart() {
+  setLocalStorage("so-cart", product);
+}
 
 function renderProductDetails(product) {
   document.querySelector("#productName").innerText = product.Brand.Name;
-  document.querySelector("#productNameWithoutBrand").innerText = product.NameWithoutBrand;
+  document.querySelector("#productNameWithoutBrand").innerText =
+    product.NameWithoutBrand;
   document.querySelector("#productImage").src = product.Image;
   document.querySelector("#productImage").alt = product.Name;
   document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
-  document.querySelector("#productColorName").innerText = product.Colors[0].ColorName;
-  document.querySelector("#productDescriptionHtmlSimple").innerHTML = product.DescriptionHtmlSimple;
+  document.querySelector("#productColorName").innerText =
+    product.Colors[0].ColorName;
+  document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+    product.DescriptionHtmlSimple;
   document.querySelector("#addToCart").dataset.id = product.Id;
-}
-
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
 }
 
 export { productDetails }; // Export the productDetails function
